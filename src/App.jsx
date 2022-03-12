709e28fecb803e5e574598df37452a26fe51a494
Slaven B <slaven.bunijevac@gmail.com>
2022-03-12 23:58:22 +0100

    add input change handler

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,26 @@ import texts from './data';
 function App() {
 
   const [ data, setData ] = useState(texts);
+  const [ textNumber, setTextNumber ] = useState(0);
+
+  const generateText = e => {
+    e.preventDefault();
+  }
+
+  const numberInputHandler = e => {
+    const number = +e.target.value;
+    setTextNumber(number);
+  }
 
   return (
     <div className="App">
       <h2 className="title">Generate text</h2>
-      <form className="form">
-        <input type="number" className="form-input" />
+      <form className="form" onSubmit={generateText}>
+        <input
+          onChange={numberInputHandler}
+          type="number"
+          value={textNumber}
+          className="form-input" />
         <button className="form-button">generate</button>
       </form>
     </div>
