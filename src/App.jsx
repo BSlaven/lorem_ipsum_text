b287a716cc1e415edfc52c5cb1c7d1b90f02a7cb
Slaven B <slaven.bunijevac@gmail.com>
2022-03-12 22:57:45 +0100

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import text from './data';
+
+function App() {
+
+  console.log(text);
+
+  return (
+    <div className="App">
+
+    </div>
+  )
+}
+
+export default App
